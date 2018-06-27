a44a2e9c82ba20f5af57eb5506a0ccf67bb403a8
Владимир <corpsess@bk.ru>
2018-06-27 21:56:38 +0400

    Публикация данных на веб страничке
    Публикация данных на веб страничке

---
+++ interaction_with_html_pages/src/main/java/ru/netcracker/interaction_with_bd/InteractionWithBdApplication.java
@@ -0,0 +1,12 @@
+package ru.netcracker.interaction_with_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractionWithBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractionWithBdApplication.class, args);
+	}
+}
